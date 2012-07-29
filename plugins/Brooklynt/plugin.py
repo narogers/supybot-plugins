d95962cf202de886d6fa78d5dcb01cdced454aa6
Mark A. Matienzo <mark@matienzo.org>
2012-07-29 17:27:00 -0400

    brooklynt: hand-crafted artisanal integers from brooklyn

---
+++ plugins/Brooklynt/plugin.py
@@ -0,0 +1,63 @@
+###
+# Copyright (c) 2010, Michael B. Klein
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+import supybot.utils.web as web
+
+from urllib import urlencode
+import urlib2
+import simplejson
+
+HEADERS = dict(ua = 'Zoia/1.0 (Supybot/0.83; Brooklynt Plugin; http://code4lib.org/irc)')
+
+class Brooklynt(callbacks.Plugin):
+    
+    def __init__(self):
+        self.url = "http://api.brooklynintegers.com/rest/"
+
+    def brooklynt(self, irc, msg, args, query):
+      """Request a new hand-crafted artisinal integer from http://brooklynintegers.com"""
+      params = {'method': 'brooklyn.integers.create'}
+      data = urlencode(params)
+      request = urllib2.Request(url, data)
+      response = simplejson.loads(urllib2.urlopen(req))
+      result = "Your hand-crafted integer is %s - %s " % (response.integer, response.shorturl)
+      irc.reply(result)
+        
+    brooklyninteger = brooklynt
+     
+Class = Brooklynt
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
