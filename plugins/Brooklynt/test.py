5dc0ea5af14fc39fdf45198877d5b28d1381723d
Mark A. Matienzo <mark@matienzo.org>
2012-07-29 17:53:14 -0400

    brooklynt: ya rly, fix boilerplate

--- plugins/Brooklynt/test.py
+++ plugins/Brooklynt/test.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2010, Michael B. Klein
+# Copyright (c) 2012, Mark A. Matienzo
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,8 +30,8 @@
 
 from supybot.test import *
 
-class WundergroundTestCase(PluginTestCase):
-    plugins = ('Wunderground',)
+class BrooklyntTestCase(PluginTestCase):
+    plugins = ('Brooklynt',)
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
