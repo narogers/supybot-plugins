24462ebfc943bf4f1cb409b81f1742b8f4e83b9a
jtgorman <jonathan.gorman@gmail.com>
2013-02-03 08:41:45 -0600

    Added nick of bot to the greeting message

--- plugins/Greeter/plugin.py
+++ plugins/Greeter/plugin.py
@@ -40,7 +40,11 @@ import supybot.conf as conf
 import re 
 
 filename = conf.supybot.directories.data.dirize('Greeter.db')
-joinmsg = "Welcome to code4lib! Visit http://code4lib.org/irc to find out more about this channel.  Type @helpers #code4lib for a list of people in channel who can help."
+
+# ok, could use {} to replace channel and have a
+# generic method and have code4lib be a more specific one
+
+joinmsg = "Welcome to #code4lib! I'm {}, the channel bot. Visit http://code4lib.org/irc to find out more about this channel.  Type @helpers #code4lib for a list of people in channel who can help."
 
 # drawn in part from Herald and Seen
 class GreeterDB(plugins.ChannelUserDB):
@@ -138,7 +142,7 @@ class Greeter(callbacks.Plugin):
         if len(args) == 0:
             if ircutils.strEqual(irc.nick, msg.nick):
                 return # It's us
-            irc.queueMsg(ircmsgs.privmsg(msg.nick, joinmsg))
+            irc.queueMsg(ircmsgs.privmsg(msg.nick, joinmsg.format( irc.nick )))
             irc.noReply()
         elif args[0] == 'remove':
             self.__remove(irc, channel, args[1:])
@@ -164,7 +168,7 @@ class Greeter(callbacks.Plugin):
         try:
             self.db.get(channel, msg.nick)
         except KeyError:
-            irc.queueMsg(ircmsgs.privmsg(msg.nick, joinmsg))
+            irc.queueMsg(ircmsgs.privmsg(msg.nick, joinmsg.format( irc.nick ) ))
             irc.noReply()
             #self.db.add(channel, msg.nick)
             self.db.add(channel, msg.nick) 
